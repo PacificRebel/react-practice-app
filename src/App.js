291c33fd5946bf62102dd74ad0c6e5238d777598
Heli Sivunen <heli@mooncat.tv>
2019-11-12 16:43:25 +0000

    first commit - setting up React app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. Amazing - you don't have to hit the refresh button!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React or you'll be a big loser!'
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
