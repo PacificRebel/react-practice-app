027fad2a86a16cea096a16829c9e5a27b1543225
Heli Sivunen <heli@mooncat.tv>
2019-11-13 13:26:32 +0000

    Working app with a tiny counter at the top of the screen.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Amazing - you don't have to hit the refresh button!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React or you'll be a big loser!'
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Counter />
+      </div>
+    );
+  }
+}
+
+class Counter extends Component {
+  state = {
+    count: 0
+  }
+  handleClick = () => {
+    this.setState((prevState, { count }) => ({
+      count: prevState.count + 1
+    }))
+  }
+  render() {
+    return <button onClick={this.handleClick}>{this.state.count}</button>;
+  }
 }
 
 export default App;
+
+//
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload. Hello!
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React or you'll be a big loser!'
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
